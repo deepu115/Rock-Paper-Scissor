8b3d0fcbc0132145703a26ba1025326d6e7d03ce
Dileep <dileepvunnam@gmail.com>
2023-07-31 23:04:17 +0100

    option test passed

--- src/Player.js
+++ src/Player.js
@@ -2,12 +2,14 @@ class Player {
     #name
     constructor(name) {
         this.#name = name;
-
+        this.option = null;
     }
     getName() {
         return this.#name;
     }
-
+    chooseOption(option) {
+        this.option = option;
+    }
 
 }
 
