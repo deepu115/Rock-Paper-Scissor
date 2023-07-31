b08c51c828be713f13a4ace95b5edd0f2c7d52c8
Dileep <dileepvunnam@gmail.com>
2023-07-31 23:15:24 +0100

    winner test passed

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,11 @@ class Game {
         this.player1 = player1;
         this.player2 = player2;
     }
+    play() {
+        const rules = { 'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper' };
+        this.winner = rules[this.player1.option] === this.player2.option ? this.player1 : this.player2;
+    }
 }
+
 
 module.exports = Game;
