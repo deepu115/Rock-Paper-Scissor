2a17afc04cc1756abcd355f1b098e77723e8d778
Dileep <dileepvunnam@gmail.com>
2023-08-01 21:20:02 +0100

    game route

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 const indexRouter = require('./routes/index.js');
-// const gameRouter = require('./routes/game.js');
+const gameRouter = require('./routes/game.js');
 // const turnRouter = require('./routes/turn.js');
 
 app.use('/', indexRouter);
-// app.use('/game', gameRouter);
+app.use('/game', gameRouter);
 // app.use('/turn', turnRouter);
 
 app.listen(port, () => {
