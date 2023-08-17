dbc3e0a3650646d3aa11fcf6097a58dfcd0ecdc1
Dileep <dileepvunnam@gmail.com>
2023-08-17 14:42:41 +0100

    another commit

--- app.js
+++ app.js
@@ -8,15 +8,22 @@ app.use(express.static('public'));
 
 
 import index from './routes/index.js';
+import home from './routes/home.js';
+import multiPlay from './routes/multiPlay.js';
 import play from './routes/play.js';
 import result from './routes/result.js';
+import multiResult from './routes/multiResult.js';
 
-
+app.use('/', home);
 app.use('/', index);
 app.use('/', play);
+app.use('/', multiPlay);
 app.use('/', result);
+app.use('/', multiResult);
+
 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
+export default app;
